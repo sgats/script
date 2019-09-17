904291152f5534b5e9a621cf6c004ea1c0f05dd7
sgats <yanyu_1988@yeah.net>
2019-09-17 14:32:35 +0800

    Create CRS-just-package.sh
    新的配置信息

---
+++ CRS-just-package.sh
@@ -0,0 +1,7 @@
+core_interface=http://10.50.180.66:8080/jenkins/view/CRS/job/$select/$build/com-interface'$'core-interface/artifact/com-interface/core-interface/0.0.1-SNAPSHOT/core-interface-0.0.1-SNAPSHOT.tar.gz
+core_quartz=http://10.50.180.66:8080/jenkins/view/CRS/job/$select/$build/core-quartz'$'core-quartz/artifact/core-quartz/core-quartz/0.0.1-SNAPSHOT/core-quartz-0.0.1-SNAPSHOT.tar.gz
+core_quartz_deposit=http://10.50.180.66:8080/jenkins/view/CRS/job/$select/$build/core-quartz-deposit'$'core-quartz-deposit/artifact/core-quartz-deposit/core-quartz-deposit/0.0.1-SNAPSHOT/core-quartz-deposit-0.0.1-SNAPSHOT.tar.gz
+core_quartz_pay=http://10.50.180.66:8080/jenkins/view/CRS/job/$select/$build/core-quartz-pay'$'core-quartz-pay/artifact/core-quartz-pay/core-quartz-pay/0.0.1-SNAPSHOT/core-quartz-pay-0.0.1-SNAPSHOT.tar.gz
+core_schedule=http://10.50.180.66:8080/jenkins/view/CRS/job/$select/$build/core-schedule'$'core-schedule/artifact/core-schedule/core-schedule/0.0.1-SNAPSHOT/core-schedule-0.0.1-SNAPSHOT.tar.gz
+core_web=http://10.50.180.66:8080/jenkins/view/CRS/job/$select/$build/core-web'$'core-web/artifact/core-web/core-web/0.0.1-SNAPSHOT/core-web-0.0.1-SNAPSHOT.tar.gz
+core_web_pub=http://10.50.180.66:8080/jenkins/view/CRS/job/$select/$build/core-web-pub'$'core-web-pub/artifact/core-web-pub/core-web-pub/0.0.1-SNAPSHOT/core-web-pub-0.0.1-SNAPSHOT.tar.gz
